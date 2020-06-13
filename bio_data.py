0674f39e35556c6683532ee3ab5437fce918eef4
nurse_mimi <mimzzy.co@gmail.com>
2020-06-13 21:25:05 +1400

    biodata

---
+++ bio_data.py
@@ -0,0 +1,22 @@
+name          = "Miracle"
+location      = "Anambra"
+occupation    = "nursing student"
+skills        = "for some reason i can not think of one"
+fact          = "Manga is life"
+email         = "mimzzy.co@gmail.com"
+
+print("my name is " +name+ ", i stay in " +location+ ", i am a " +occupation+ ", my skills include " +skills+ ", A fact of life is that " +fact+ ", My email address is " +email)
+
+name          = "Miracle"
+location      = "Anambra"
+occupation    = "nursing student"
+skills        = "for some reason i can not think of one"
+fact          = "Manga is life"
+email         = "mimzzy.co@gmail.com"
+
+print(name)
+print(location)
+print(occupation)
+print(skills)
+print(fact)
+print(email)
---
+++ version1.py
@@ -0,0 +1,4 @@
+import sys  #sys (system) is imported first so that python can be able to assess it
+print(sys.version)  #'print' enables python to interpret the command
+                   #(sys.vertion) is the command, and it is typed without quotes("").
+                   
