8b7a30a8853a2f36f06295c5af096499a01578ff
nurse_mimi <mimzzy.co@gmail.com>
2020-06-13 16:05:41 +1400

    python identation

--- python.py
+++ python.py
@@ -1,3 +1,34 @@
 print("HELLO WORLD")
 print("sage")
 print("HELLO, PYTHON")
+
+#!/bin/python
+if True:
+    print("Sterial")
+else:
+    print("Nonsterial")
+
+print('word')
+print("this is a sentence")
+print("""this is a paragraph. it is made
+up of multiple lines and sentences""")
+
+# First comment
+print("Hello, Mimi!") # second comment
+
+# this is a comment.
+# this is a comment, too.
+# this is also a comment.
+# I said that already.
+
+counter = 100         # an integer assignment
+miles   = 1000.0      # A floating point
+name    = "Mimi"      # A string
+
+print(counter)
+print(miles)
+print(name)
+
+a = b = c = 1
+
+a,b,c = 1,2,"Mimi"
